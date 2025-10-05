79eda3c20a8c51e143033380bf9d9c72eefd3d34
Do1904 <s04bento1222@gmail.com>
2025-10-05 17:50:30 +0900

    ヘッダーが邪魔だったので基本表示されていないようにした

--- myNewApp/src/app/components/app/app.component.ts
+++ myNewApp/src/app/components/app/app.component.ts
@@ -3,17 +3,12 @@ import { StickerComponent } from '../sticker/sticker.component';
 import { RouterModule } from '@angular/router';
 import { DataService } from '../../service/data.service'; // Import the data service
 import { User } from '../../model/user';
-import { HeaderComponent } from "../header/header.component";
-import { FooterComponent } from "../footer/footer.component"; // Import the user model
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component'; // Import the user model
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    StickerComponent,
-    RouterModule,
-    HeaderComponent,
-    FooterComponent
-  ],
+  imports: [StickerComponent, RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css', '../../../sticker.css'],
 })
@@ -21,19 +16,31 @@ export class AppComponent implements OnInit {
   title = 'Stickers';
   data: User[] = []; // Property to store the data
 
-  constructor(private dataService: DataService) { } // Inject the data service
+  constructor(private dataService: DataService) {} // Inject the data service
 
   ngOnInit(): void {
-    this.dataService.getData().subscribe(data => {
-      this.data = data; // Assign the received data to the property
-    }, (error) => {
-      console.error('There was an error retrieving data:', error);
-    });
+    this.dataService.getData().subscribe(
+      (data) => {
+        this.data = data; // Assign the received data to the property
+      },
+      (error) => {
+        console.error('There was an error retrieving data:', error);
+      }
+    );
   }
 
   public isHeaderVisible = false;
 
   toggleHeader() {
     this.isHeaderVisible = !this.isHeaderVisible;
+  }
+
+  onHeaderEnter() {
+    // カーソルがヘッダー領域に入ったら何もしない（開いたまま）
+  }
+
+  onHeaderLeave() {
+    // カーソルが115px領域を離れたら閉じる
+    this.isHeaderVisible = false;
   }
 }
